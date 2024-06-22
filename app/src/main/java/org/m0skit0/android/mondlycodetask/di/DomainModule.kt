a24577c90a06d8a86b67384a271256c8ef219ae0
Yassine Diouri <ametric@gmail.com>
2024-06-22 13:47:01 +0200

    Fixed item repository with interface + implementation

--- app/src/main/java/org/m0skit0/android/mondlycodetask/di/DomainModule.kt
+++ app/src/main/java/org/m0skit0/android/mondlycodetask/di/DomainModule.kt
@@ -2,10 +2,11 @@ package org.m0skit0.android.mondlycodetask.di
 
 import org.koin.dsl.module
 import org.m0skit0.android.mondlycodetask.domain.ItemsRepository
+import org.m0skit0.android.mondlycodetask.domain.ItemsRepositoryImpl
 
 val domainModule = module {
 
-    single {
-        ItemsRepository(get())
+    single<ItemsRepository> {
+        ItemsRepositoryImpl(get())
     }
 }
--- app/src/main/java/org/m0skit0/android/mondlycodetask/domain/ItemsRepository.kt
+++ app/src/main/java/org/m0skit0/android/mondlycodetask/domain/ItemsRepository.kt
@@ -3,8 +3,21 @@ package org.m0skit0.android.mondlycodetask.domain
 import org.m0skit0.android.mondlycodetask.data.ItemsDTO
 import org.m0skit0.android.mondlycodetask.data.ItemsService
 
-class ItemsRepository(
+interface ItemsRepository {
+    suspend fun items(): List<ItemsDTO>
+}
+
+class ItemsRepositoryImpl(
     private val itemsService: ItemsService,
-) {
-    suspend fun items(): List<ItemsDTO> = itemsService.items()
+) : ItemsRepository {
+
+    // Trivial caching mechanism to avoid multiple network calls
+    private var cachedItems: List<ItemsDTO> = emptyList()
+
+    override suspend fun items(): List<ItemsDTO> {
+        if (cachedItems.isEmpty()) {
+            cachedItems = itemsService.items()
+        }
+        return cachedItems
+    }
 }
