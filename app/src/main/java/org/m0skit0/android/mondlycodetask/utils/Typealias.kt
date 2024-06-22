89b1511a7b2ad20b088fe71d517f0cfc22daaa4e
Yassine Diouri <ametric@gmail.com>
2024-06-22 16:06:47 +0200

    Added UiState class for UI states

--- app/src/main/java/org/m0skit0/android/mondlycodetask/utils/Typealias.kt
+++ app/src/main/java/org/m0skit0/android/mondlycodetask/utils/Typealias.kt
@@ -1,5 +1,9 @@
 package org.m0skit0.android.mondlycodetask.utils
 
+import org.m0skit0.android.mondlycodetask.domain.Item
+
 typealias Url = String
 typealias Id = String
+
+typealias Items = List<Item>
 
