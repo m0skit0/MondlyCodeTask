420fb4c18d94562ecbdb2c300075ce30368b853c
Yassine Diouri <ametric@gmail.com>
2024-06-24 14:12:44 +0200

    Moved service call to IO dispatcher

--- app/src/main/java/org/m0skit0/android/mondlycodetask/domain/ItemsRepository.kt
+++ app/src/main/java/org/m0skit0/android/mondlycodetask/domain/ItemsRepository.kt
@@ -1,5 +1,8 @@
 package org.m0skit0.android.mondlycodetask.domain
 
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import org.m0skit0.android.mondlycodetask.data.ItemsService
 
 interface ItemsRepository {
@@ -9,6 +12,7 @@ interface ItemsRepository {
 class ItemsRepositoryImpl(
     private val itemsService: ItemsService,
     private val domainItemMapper: DomainItemMapper,
+    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO,
 ) : ItemsRepository {
 
     // Trivial caching mechanism to avoid multiple network calls
@@ -16,7 +20,7 @@ class ItemsRepositoryImpl(
 
     override suspend fun items(): Result<List<Item>> = runCatching {
         if (cachedItems.isEmpty()) {
-            val items = itemsService.items()
+            val items = withContext(ioDispatcher) { itemsService.items() }
             cachedItems = domainItemMapper.map(items)
         }
         cachedItems
