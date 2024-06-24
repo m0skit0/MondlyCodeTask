68425a40b837723a6e507523414c07e526bb1c0b
Yassine Diouri <ametric@gmail.com>
2024-06-24 13:03:21 +0200

    Disable edge to edge and make loading a proper animation

--- app/src/main/java/org/m0skit0/android/mondlycodetask/presentation/MainActivity.kt
+++ app/src/main/java/org/m0skit0/android/mondlycodetask/presentation/MainActivity.kt
@@ -9,7 +9,6 @@ import org.m0skit0.android.mondlycodetask.presentation.theme.MondlyCodeTaskTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             MondlyCodeTaskTheme {
                 MainItemListComposable()
--- app/src/main/java/org/m0skit0/android/mondlycodetask/presentation/MainItemListComposable.kt
+++ app/src/main/java/org/m0skit0/android/mondlycodetask/presentation/MainItemListComposable.kt
@@ -1,11 +1,18 @@
 package org.m0skit0.android.mondlycodetask.presentation
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.koin.androidx.compose.koinViewModel
 import org.m0skit0.android.mondlycodetask.domain.Item
 
@@ -14,16 +21,23 @@ fun MainItemListComposable(
     viewModel: ItemListViewModel = koinViewModel(),
 ) {
     val state = remember { viewModel.items }
-    when (val uiState = state.value) {
-        is UiState.Loading -> Text(text = "Loading")
-        is UiState.Success -> ItemListComposable(items = uiState.data)
-        is UiState.Error -> Text(text = "Error: ${uiState.message}")
+    Box(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        contentAlignment = Alignment.Center,
+    ) {
+        when (val uiState = state.value) {
+            is UiState.Loading -> Loading()
+            is UiState.Success -> ItemListComposable(items = uiState.data)
+            is UiState.Error -> Text(text = "Error: ${uiState.message}")
+        }
     }
 }
 
 @Composable
 private fun ItemListComposable(items: List<Item>) {
-    Column {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
         items.forEach { item ->
             ItemComposable(item)
         }
@@ -54,7 +68,7 @@ private fun ItemListComposablePreview() {
 @Composable
 private fun ItemComposable(item: Item) {
     Row {
-        Text(text = item.id)
+        Text(text = item.name)
         Text(text = item.description)
         Text(text = item.imageUrl)
     }
@@ -71,4 +85,9 @@ private fun ItemComposablePreview() {
             imageUrl = "imageUrl",
         )
     )
+}
+
+@Composable
+private fun Loading() {
+    CircularProgressIndicator()
 }
